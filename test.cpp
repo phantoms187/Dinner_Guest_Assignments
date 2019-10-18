a85b8971e6e2d25f418f154b3fe1477081be2a00
phantoms187 <dwd2@pdx.edu>
2019-10-18 13:07:20 -0700

    Initial

---
+++ test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+
+
+
+
+
+int main()
+{
+  int total_people = 0;
+
+  string line;
+  ifstream myfile ("text.txt");
+  if(myfile.is_open())
+  {
+    myfile >> total_people;
+    int people[total_people][total_people];
+
+    for(int i = 0; i < total_people; ++i)
+    {
+      for(int j = 0; j < total_people; ++j)
+      {
+        myfile >> people[i][j];
+        cout << people[i][j] << " ";
+         if(j == total_people-1)
+           cout << "\n";
+      }
+
+    }
+    cout << "Person 1 likes person 2: " << people[0][1] << "\n";
+    cout << "Person 3 likes person 7: " << people[2][6] << "\n";
+
+
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+
+  cout << "\nTotal people: " << total_people << "\n";
+
+
+
+  return 0;
+
+}
