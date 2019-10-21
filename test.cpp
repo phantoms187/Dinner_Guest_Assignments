c046110104f366c494cbafe94d2843f3003593b8
phantoms187 <dwd2@pdx.edu>
2019-10-21 02:28:06 -0700

    Scoring working setup complete

--- test.cpp
+++ test.cpp
@@ -8,23 +8,32 @@ int table_likeness_score(int chairs, int **people, int assignment[])
 {
   int sum = 0;
   int half_chairs = chairs/2;
+  int current_chair = -1;
+  int last_chair = -1;
 
+//Calculate scores for people sitting next to each other
   for(int i = 0; i < 2; ++i)
   {
     for(int j = 0; j < half_chairs-1; ++j)
     {
-      sum += people[assignment[(half_chairs*i)+j]][assignment[(half_chairs*i)+j+1]];
-      cout << "\nsum: " << sum << "\nAssignment: " << assignment[(half_chairs*i)+j];
+      current_chair = (half_chairs*i)+j;
+      last_chair = (half_chairs*(i+1)-(j+1));
 
-      sum += people[assignment[(half_chairs*(i+1)-(j+1))]][assignment[(half_chairs*(i+1)+(j-2))]];
-      cout << "\nsum after: " << sum << "\nAssignment: " << assignment[(half_chairs*(i+1)-(j+1))];
+      sum += people[assignment[current_chair]][assignment[current_chair+1]];
+      sum += people[assignment[last_chair]][assignment[last_chair-1]];
     }
   }
-  
+//Add scores for people across from each other
+  for(int i = 0; i < half_chairs; ++i)
+  {
+    current_chair = i;
+
+    sum += people[assignment[current_chair]][assignment[current_chair+half_chairs]];
+    sum += people[assignment[current_chair+half_chairs]][assignment[current_chair]];
+  }
+
   return sum;
 }
-
-
 
 int main(int argc, char** argv)
 {
@@ -48,25 +57,24 @@ int main(int argc, char** argv)
       for(int j = 0; j < chairs; ++j)
       {
         myfile >> people[i][j];
-        /*cout << people[i][j] << " ";
-         if(j == chairs-1)
-           cout << "\n";
-        */
       }
-
     }
 
     myfile.close();
   }
-
   else cout << "Unable to open file";
 
   cout << "\nPerson 3 likes Person 5 this much: " << people[2][4] << "\n";
 
   cout << "\nTotal people: " << chairs << "\n";
-  
-  int assignment[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; 
-  
+
+  int assignment[chairs];// = {0,5,1,6,2,7,3,8,4,9};
+
+  for(int i = 0; i < chairs; ++i)
+  {
+     assignment[i] = i;
+  }
+
   int sum = table_likeness_score(chairs, people, assignment);
 
   cout << "\nSum is: " << sum << "\n";
